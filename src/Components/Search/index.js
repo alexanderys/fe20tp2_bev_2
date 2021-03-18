db7c3fb8cc881d422822c93a74bcd91e4e0f42de
alexanderys <alexander.yararsellgren@student.kyh.se>
2021-03-18 15:25:07 +0100

    :sparkles: search movies, cleaned code

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -2,16 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { withAuthorization } from '../Session';
 import axios from 'axios';
 
+const IMAGE_URL = "https://image.tmdb.org/t/p/original";
+
 function Search() {
     const [searchTerm, setSearchTerm] = useState('');
     const [movies, setMovies] = useState([]);
-    const [submitted, setSubmitted] = useState(null);
 
     const onInputChange = (e) => {
         e.preventDefault();
 
         setSearchTerm(e.target.value);
-
 
         fetch(`https://api.themoviedb.org/3/search/movie?api_key=0b990be39bf553eaa0eaaba70e328081&language=en-US&page=1&include_adult=false&query=${e.target.value}`)
             .then((res) => res.json())
@@ -39,7 +39,14 @@ function Search() {
             {movies.length > 0 && (
                 <ul>
                     {movies.map((movie) => (
-                        <li>{movie.title}</li>
+                        <li key={movie.id}>
+                            <img style={{ width: 70 }}
+                                src={IMAGE_URL + movie.poster_path}
+                                alt={movie.title}
+                            />
+                            <h3>{movie.title}</h3>
+                            <p>{movie.release_date}</p>
+                        </li>
                     ))}
                 </ul>
             )}
