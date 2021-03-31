4bdaa864816c54cceedec19f0509adcb9cf1459c
karlG <kallegunnarsson@hotmail.com>
2021-03-31 10:53:00 +0200

    Put consts in separate file and messed up watched

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -2,26 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import MovieItem from "./Search/MovieItem";
 import styled from "styled-components";
-
-/**API KEY:  076af6ad004d95e72faff607aff3810e (Guns)
-export const API_URL = "https://api.themoviedb.org/3";
-export const NOW_PLAYING = "/movie/now_playing?api_key=";
-export const MOST_POPULAR = "/movie/popular?api_key=";
-export const TOP_RATED = "/movie/top_rated?api_key=";
-export const API_KEY = "076af6ad004d95e72faff607aff3810e";
-export const SETTINGS = "&language=en-US&page=1";
-export const IMAGE_URL = "https://image.tmdb.org/t/p/original";
-export const SEARCH_API =
-  'https://api.themoviedb.org/3/search/movie?api_key=1b0fe85392a0de289e401e2f384cf20b&query="';
-/*   
-const SEARCH_API =
-  'https://api.themoviedb.org/3/search/movie?api_key=1b0fe85392a0de289e401e2f384cf20b&query="';
- */
-/* movies.forEach((movie) => {
-  const { title, poster_path, vote_average, overview } = movie;
-});
-// const NOW_PLEJJING = `/movie/now_playing?api_key=${URL_KEY}&language=en-US&page=1`
-/* https://api.themoviedb.org/3/movie/now_playing?api_key=<<api_key>>&language=en-US&page=1 */
+import * as CONSTS from '../constants/consts';
 
 const MovieListGrid = styled.main`
   display: flex;
@@ -32,16 +13,19 @@ const MovieListGrid = styled.main`
 `;
 
 const MovieList = () => {
-  const BASE_URL = "https://api.themoviedb.org/3";
-  const NOW_PLAYING = "/movie/now_playing?api_key=";
-  const API_KEY = "076af6ad004d95e72faff607aff3810e";
-  const SETTINGS = "&language=en-US&page=1";
-  const IMAGE_URL = "https://image.tmdb.org/t/p/w1280";
+
+  //INFO = These consts are now declared in the file consts.js instead
+
+  // const BASE_URL = "https://api.themoviedb.org/3";
+  // const NOW_PLAYING = "/movie/now_playing?api_key=";
+  // const API_KEY = "076af6ad004d95e72faff607aff3810e";
+  // const SETTINGS = "&language=en-US&page=1";
+  // const IMAGE_URL = "https://image.tmdb.org/t/p/w1280";
 
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    axios.get(BASE_URL + NOW_PLAYING + API_KEY + SETTINGS).then((res) => {
+    axios.get(CONSTS.BASE_URL + CONSTS.NOW_PLAYING + CONSTS.API_KEY + CONSTS.SETTINGS).then((res) => {
       setMovies(res.data.results);
       //In case of trouble, the tutorial wrote a map here!!
       // setMovies(res.data.results.map((movie) => movie));
@@ -69,7 +53,7 @@ const MovieList = () => {
               voteAverage={vote_average}
               releaseDate={release_date}
               posterPath={poster_path}
-              imgComboPath={IMAGE_URL + poster_path}
+              imgComboPath={CONSTS.IMAGE_URL + poster_path}
             />
           )
         )}
--- src/Components/Watchlist/Watched.js
+++ src/Components/Watchlist/Watched.js
@@ -1,9 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
-import { globalContext } from "../../context/GlobalState";
-import MovieItem from "../Search/MovieItem";
+//import { globalContext } from "../../context/GlobalState";
+//import MovieItem from "../Search/MovieItem";
 import styled from "styled-components";
 import { db } from "../../firebase";
 import { useAuth } from "../../context/AuthContext";
+import axios from 'axios';
+import * as CONSTS from '../../constants/consts';
 
 const MovieListGrid = styled.section`
   display: flex;
@@ -15,26 +17,13 @@ const MovieListGrid = styled.section`
 
 export const Watched = () => {
   const IMAGE_URL = "https://image.tmdb.org/t/p/w1280";
-  const { watched } = useContext(globalContext);
+  //const { watched } = useContext(globalContext);
   const { currentUser } = useAuth();
-  const [watchedMovies, setWatchedMovies] = useState([]);
+  const [watchedMoviesId, setWatchedMoviesId] = useState([]);
+  const [TestMovies, setTestMovies] = useState();
 
-  // const GetNextReviews = async () => {
-  //   const ref = db
-  //     .collection("users")
-  //     .doc(currentUser.uid)
-  //     .collection("haveWatched");
-  //   const data = await ref.get();
-  //   console.log("this is data: " + data);
-
-  //   //data.docs = an array of all the documents
-  //   data.docs.forEach((doc) => {
-  //     const review = doc.data();
-  //     console.log("this is review: " + review);
-  //   });
-  // };
-
-  // GetNextReviews();
+  //Makes the state watchedMoviesId into an array of all the movie id's
+  //of the movies in currentUsers haveWatched list.
   useEffect(() => {
     db.collection("users")
       .doc(currentUser.uid)
@@ -44,29 +33,36 @@ export const Watched = () => {
       .then((snapshot) => {
         let documents = [];
         snapshot.docs.forEach((doc) => {
-          documents.push(doc.data().movieTitle);
+          documents.push(doc.id);
         });
-        setWatchedMovies(documents);
+        setWatchedMoviesId(documents);
       });
   }, []);
 
-  console.log(watchedMovies);
+  //Temporary API import for displaying the movies in currentUsers
+  //haveWatched list
+  axios.get(CONSTS.BASE_URL + /* Movie id ??? */ + CONSTS.API_KEY + CONSTS.SETTINGS).then((res) => {
+    //setTestMovies(res.data.results);
+    console.log(res.data.results);
+  });
 
   return (
     <div>
-      <div className="header">
+      <h1>hej</h1>
+      {/* <div className="header">
         <h1>Watched Movies</h1>
-        <h2>{currentUser.uid}</h2>
-        <h2>{watchedMovies}</h2>
+        <h2>Current user id = {currentUser.uid}</h2>
+        <br />
+
 
         <h3 className="count-pill">
-          {watched.length} {watched.length === 1 ? "Movie" : "Movies"}
+          {watchedMoviesId.length} {watchedMoviesId.length === 1 ? "Movie" : "Movies"}
         </h3>
       </div>
 
-      {watched.length > 0 ? (
+      {watchedMoviesId.length > 0 ? (
         <MovieListGrid>
-          {watched.map(
+          {TestMovies.map(
             ({
               id,
               title,
@@ -82,7 +78,7 @@ export const Watched = () => {
                 voteAverage={vote_average}
                 releaseDate={release_date}
                 posterPath={poster_path}
-                imgComboPath={IMAGE_URL + poster_path}
+                imgComboPath={CONSTS.IMAGE_URL + poster_path}
                 type="watched"
               />
             )
@@ -90,7 +86,7 @@ export const Watched = () => {
         </MovieListGrid>
       ) : (
         <h2>No movies in your list! Add some!</h2>
-      )}
+      )} */}
     </div>
   );
 };
