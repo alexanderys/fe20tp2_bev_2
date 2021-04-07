62ab984bb6384a0d7b390648095a0199120e47bc
gunkarlsson <hi@gunkarlsson.com>
2021-04-07 11:32:05 +0200

    🎨  added local API-key

--- src/constants/urlParts.js
+++ src/constants/urlParts.js
@@ -3,7 +3,6 @@ export const BASE_URL = "https://api.themoviedb.org/3";
 export const NOW_PLAYING = "/movie/now_playing?api_key=";
 export const SEARCH = "search/movie?api_key=";
 
-export const API_KEY = "076af6ad004d95e72faff607aff3810e";
+export const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 export const SETTINGS = "&language=en-US&page=1";
 export const IMAGE_URL = "https://image.tmdb.org/t/p/w1280";
-
